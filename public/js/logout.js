5be3b9565ea5056d6b87d65e0b46bba5d5c5fba7
kaihuan Huang <huanhuan@kaihuans-MBP.attlocal.net>
2022-09-19 12:54:48 -0700

    update login logout singup pages

---
+++ public/js/logout.js
@@ -0,0 +1,17 @@
+const logoutFormHandler = async(event) =>{
+
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document
+.querySelector('.logout-form')
+.addEventListener('click', logoutFormHandler);
