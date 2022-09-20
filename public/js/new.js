ffda228b78a8709f9298cc4809daa9cde67153a5
kaihuan Huang <huanhuan@kaihuans-MBP.attlocal.net>
2022-09-20 12:04:00 -0700

    update new.js

---
+++ public/js/new.js
@@ -0,0 +1,18 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const body = document
+    .querySelector('input[textarea="post-body"]')
+    .value.trim();
+
+  await fetch("/api/post", {
+    method: "POST",
+    body: JSON.stringify({ body, title }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  document.location.replace("/dashboard");
+};
+
+document.querySelector(".new-form").addEventListener("submit", newFormHandler);
