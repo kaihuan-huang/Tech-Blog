d434983c6238b68281f3f0490af6179d5fa7caef
kaihuan Huang <huanhuan@kaihuans-MBP.attlocal.net>
2022-09-21 12:21:00 -0700

    update index.js in models

--- models/index.js
+++ models/index.js
@@ -2,6 +2,15 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment')
 
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
 Post.hasMany(Comment, {
     foreignKey: 'post_id',
@@ -19,4 +28,8 @@ Comment.belongsTo(User, {
     onDelete: 'CASCADE'
 });
 
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
 module.exports = { User, Post, Comment };
