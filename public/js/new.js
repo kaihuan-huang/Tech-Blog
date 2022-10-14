59561568fb4e23c60738722e85d9acd797230371
kaihuan Huang <huanhuan@kaihuans-MacBook-Pro.local>
2022-10-14 13:51:21 -0700

    code review fixed newFormHandler

--- public/js/new.js
+++ public/js/new.js
@@ -1,19 +1,23 @@
 //create 
 const newFormHandler = async (event) => {
   event.preventDefault();
-
   const title = document.querySelector('input[name="post-title"]').value.trim();
-  const body = document
-    .querySelector('textarea[name="post-body"]')
-    .value.trim();
+  const body = document.querySelector('textarea[name="post-body"]').value.trim();
+  console.log(title);
+  console.log(body);
 
-  await fetch("/api/posts", {
+  await fetch(`/api/posts`, {
     method: "POST",
-    body: JSON.stringify({ title, body }),
-    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      title,
+      body,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
-
   document.location.replace("/dashboard");
+  
 };
 
 document.querySelector("#new-post-form").addEventListener("submit", newFormHandler);
